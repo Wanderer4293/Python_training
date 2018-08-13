6fabd3cab7d4ca5d2a2cfcab40b4f3d132ded790
orazvaliev <Ruslan.Orazvaliev@transas.com>
2018-08-13 17:34:47 +0300

    Fix descriptors.py

--- descriptors/descriptors.py
+++ descriptors/descriptors.py
@@ -14,28 +14,28 @@ class User:
 
     def __init__(self, file_name):
         self.config = ConfigHandler(file_name)
-        self._id_bytes = None
+        self._id_int = None
 
     @property
     def id_bytes(self):
         print(10 * '*' + 'Return id in bytes!' + 10 * '*')
-        return self._id_bytes
+        return self._id_int.to_bytes(4, byteorder='big', signed=False)
 
     @property
     def id_int(self):
         print(10 * '*' + 'Return id like int!' + 10 * '*')
-        return int.from_bytes(self._id_bytes, byteorder='big')
+        return self._id_int
 
-    @id_bytes.setter
-    def id_bytes(self, id):
+    @id_int.setter
+    def id_int(self, id):
         if self.config.flag:
-            self._id_bytes = id.to_bytes(4, byteorder='big', signed=False)
+            self._id_int = id
         else:
             raise Exception("Can't assigned because the flag is False. Try to switch it to `True`")
 
 
 if __name__ == '__main__':
     person = User('config.json')
-    person.id_bytes = 100
+    person.id_int = 100
     print(person.id_bytes)
     print(person.id_int)
