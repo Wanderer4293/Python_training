88bda94abed8bec7cf883da288c4e1003cccc927
orazvaliev <Ruslan.Orazvaliev@transas.com>
2018-08-13 16:29:28 +0300

    Fix time_logger and descriptors.py

--- descriptors/descriptors.py
+++ descriptors/descriptors.py
@@ -2,51 +2,41 @@ import json
 
 
 class ConfigHandler:
-    _number = None
-    _flag = None
 
-    def __init__(self):
-        self.config_file = self.open_file()
+    def __init__(self, file_name):
+        with open(file_name, 'r') as cf:
+            config_file = json.load(cf)
 
-    @property
-    def number(self):
-        if not self._number:
-            self._number = self.config_file['number'].to_bytes(4, byteorder='big', signed=False)
-        return self._number
-
-    @property
-    def flag(self):
-        if not self._flag:
-            self._flag = self.config_file['flag']
-        return self._flag
-
-    @staticmethod
-    def open_file():
-        file_path = 'config.json'
-        with open(file_path, 'r', encoding='utf-8') as config_json:
-            return json.load(config_json)
+        self.number = config_file['number']
+        self.flag = config_file['flag']
 
 
 class User:
-    _binary_id = None
 
-    def __init__(self):
-        self.config = ConfigHandler()
+    def __init__(self, file_name):
+        self.config = ConfigHandler(file_name)
+        self._id_bytes = None
 
     @property
-    def binary_id(self):
-        if not self._binary_id:
-            self._binary_id = self.config.number
-        return self._binary_id
+    def id_bytes(self):
+        print(10 * '*' + 'Return id in bytes!' + 10 * '*')
+        return self._id_bytes
 
-    @binary_id.setter
-    def binary_id(self, id):
+    @property
+    def id_int(self):
+        print(10 * '*' + 'Return id like int!' + 10 * '*')
+        return int.from_bytes(self._id_bytes, byteorder='big')
+
+    @id_bytes.setter
+    def id_bytes(self, id):
         if self.config.flag:
-            self._binary_id = id.to_bytes(4, byteorder='big', signed=False)
+            self._id_bytes = id.to_bytes(4, byteorder='big', signed=False)
         else:
-            raise Exception("Can't assigned because the flag is False. Try switching it to `True`")
+            raise Exception("Can't assigned because the flag is False. Try to switch it to `True`")
 
 
-person = User()
-person.binary_id = 34325
-print(person.binary_id)
+if __name__ == '__main__':
+    person = User('config.json')
+    person.id_bytes = 100
+    print(person.id_bytes)
+    print(person.id_int)
