beffeca948647f1041f67db87dcab880f105a2cc
nikman <nikita.manserov@wartsila.com>
2019-05-14 12:57:54 +0300

    123

---
+++ Credentials.py
@@ -0,0 +1,6 @@
+
+
+class Credentials:
+    def __init__(self, Username, Password):
+        self.username = Username
+        self.password = Password
--- UntitledTestCase0.py
+++ UntitledTestCase0.py
@@ -6,7 +6,9 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
-from Group import Group
+from Credentials import Credentials
+from Parameters import Parameters
+import random
 
 
 class UntitledTestCase(unittest.TestCase):
@@ -16,34 +18,42 @@ class UntitledTestCase(unittest.TestCase):
         self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
     def test_untitled_test_case(self):
-        driver = self.Login(Group(username="admin", password="secret"))
-        self.Add_Group(driver)
-        self.Logout(driver)
+        self.Login(Credentials(Username="admin", Password="secret"))
+        self.Add_Group(self.driver, Parameters(GroupName=random.randint(1, 1000000),
+                                               GroupHeader=random.randint(1, 1000000),
+                                               GroupFooter=random.randint(1, 1000000)))
+        self.Logout()
         time.sleep(5)
 
-    def Logout(self, driver):
-        driver.find_element_by_link_text("Logout").click()
+    def Logout(self):
+        self.driver.find_element_by_link_text("Logout").click()
 
-    def Add_Group(self, driver):
+    def Add_Group(self, driver, parameters):
         driver.find_element_by_link_text("groups").click()
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("123444")
+        driver.find_element_by_name("group_name").send_keys(parameters.GroupName)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(parameters.GroupName)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(parameters.GroupName)
         driver.find_element_by_name("submit").click()
         driver.find_element_by_link_text("group page").click()
 
-    def Login(self, group):
+    def Login(self, cred):
         driver = self.driver
         driver.get("http://localhost:81/addressbook/index.php")
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(group.username)
+        driver.find_element_by_name("user").send_keys(cred.username)
         driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(group.password)
+        driver.find_element_by_name("pass").send_keys(cred.password)
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
         return driver
